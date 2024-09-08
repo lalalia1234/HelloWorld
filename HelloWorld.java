e7bcd554752981d731fe846ad1a535d2e0319fba
Eulalia Pedro <epedro1218@gmail.com>
2024-09-08 15:38:52 -0700

    made changes to code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World.");
+        System.out.println("Hello, World. My name is Eulalia Pedro.");
     }
 }
